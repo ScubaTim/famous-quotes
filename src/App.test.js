982af0037486a5ebab23f89c30989b54d55bb18e
Tim Gentry <gentry.tim@gmail.com>
2020-11-04 17:46:15 -0500

    enzyme added, first test passing, app component renders

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test='${val}']`);
+};
+
+test('renders the App component', () => {
+    const wrapper = setup();
+    const appComponent = findByTestAttr(wrapper, "component-app");
+    expect(appComponent.length).toBe(1);
+})
+
+test('renders button', () => {
+
+})
+
+test('connects to quotes API', () => {
+
+})
+
+test('recieves data from quotes API', () => {
+
+})
+
+test('renders data from quote API', () => {
+
+})
+
+test('generates new quote when button is clicked', () => {
+
+})
