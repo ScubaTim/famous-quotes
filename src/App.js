a1008b78f86007107953472e4882f97fd555356e
Tim Gentry <gentry.tim@gmail.com>
2020-11-05 19:06:16 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
   Card, CardText, CardBody,
-  CardTitle, Button
+  CardTitle, CardSubtitle, Button
 } from 'reactstrap';
 
 
 function App() {
   const [randomQuotes, setRandomQuotes] = useState([])
   const [quote, setQuote] = useState({
-    text: "Randomized quotation of a famous person.",
+    text: "Quotations from famously known people, presented at random from a curated list.",
     author: ""
   });
 
@@ -30,19 +30,31 @@ function App() {
   console.log(quote)
 
   return (
-    <div data-test="component-app">
-      <Card data-test="component-card" body className="mx-auto mt-5" style={{minWidth:"24rem", width:"50vw"}}>
+    <div data-test="component-app" className="bg-primary d-flex justify-content-center align-items-center" style={{height: "100vh"}}>
+      <div className="shadow">
+      <Card data-test="component-card" color="light" style={{minWidth:"320px", width:"30vw"}}>
         <CardBody>
-          <CardTitle tag="h5">A Random Famous Quote</CardTitle>
-          <div>
+          <CardTitle tag="h1" className="text-center text-uppercase my-2">A Famous Quote</CardTitle>
+          <CardSubtitle className="text-center text-uppercase font-weight-bold text-secondary">Randomly Selected</CardSubtitle>
+          <hr className="mt-3" />
+          <div className="px-4 pt-4 pb-5">
             <CardText data-test="component-quote">
               {quote.text}
             </CardText>
-            <p className="text-right">{quote.author ? `~ ${quote.author}` : ''}</p>
+            <p className="text-right px-4">{quote.author ? `~ ${quote.author}` : ''}</p>
           </div>
-          <Button data-test="component-button" onClick={() => getRandomQuote()}>Button</Button>
+          <div className="d-flex mx-4 mb-4">
+          <Button color="success" block data-test="component-button" onClick={() => getRandomQuote()}>Random Quote</Button>
+          </div>
+          <div>
+            <hr />
+            <h6 className="text-secondary px-4">
+              <em>Quotations api provided by <a href="https://type.fit/">type.fit</a></em>
+            </h6>
+          </div>
         </CardBody>
       </Card>
+      </div>
       </div>
   );
 }
